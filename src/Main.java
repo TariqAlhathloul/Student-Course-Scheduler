a00a3bbe952d128c0258b4f1bd31617e844a10d3
Tariq Hassan <145034954+TariqAlhathloul@users.noreply.github.com>
2024-04-24 19:21:08 +0300

    initial comment

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+
+/*
+* subject: IT-245 Project.
+* student name: Tariq Hassan.
+* CRN: 21008.
+* Date: 2024 Arp 24
+* problem selected: Student Course Scheduler.
+* */
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
