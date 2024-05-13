e1e173490a03d7b11abf0da33641a011fd2c4ef7
Tariq <145034954+TariqAlhathloul@users.noreply.github.com>
2024-05-13 15:00:17 +0300

    fix some errors

--- src/CourseScheduler/Main.java
+++ src/CourseScheduler/Main.java
@@ -2,6 +2,7 @@ package CourseScheduler;
 
 import java.util.HashMap;
 import java.io.*;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.Map;
 
@@ -26,37 +27,36 @@ public class Main{
         logInOption = input.nextInt();
 
         while (logInOption != 0) {
+            try {
 
-            switch (logInOption) {
-                case 1 -> {
-                    System.out.println();
-                    System.out.println(Messages.studentMessage);
-                    Student student = new Student();
-                    student.studentINFO();
-                    logInOption = input.nextInt();
-                    while (logInOption != 0) {
+
+                switch (logInOption) {
+                    case 1 -> {
+                        System.out.println();
+                        System.out.println(Messages.studentMessage);
+                        Student student = new Student();
+                        student.studentINFO();
                         logInOption = input.nextInt();
-                        switch (logInOption) {
-                            case 1 -> {
-                                System.out.println("View Course Schedule");
-                                course.enrolledCourses.forEach(System.out::println);
-                                break;
-                            }
-                            case 2 -> {
-                                System.out.println("View Available Courses");
-                                Course showCourse = new Course();
-                                showCourse.displayCourses();
-                                break;
-                            }
-                            case 3 -> {
-                                System.out.println("View My Information");
-                                student.displayStudent();
-                                break;
-                            }
-                            case 4 -> {
-                                try {
-
-
+                        while (logInOption != 0) {
+                            logInOption = input.nextInt();
+                            switch (logInOption) {
+                                case 1 -> {
+                                    System.out.println("View Course Schedule");
+                                    course.enrolledCourses.forEach(System.out::println);
+                                    break;
+                                }
+                                case 2 -> {
+                                    System.out.println("View Available Courses");
+                                    Course showCourse = new Course();
+                                    showCourse.displayCourses();
+                                    break;
+                                }
+                                case 3 -> {
+                                    System.out.println("View My Information");
+                                    student.displayStudent();
+                                    break;
+                                }
+                                case 4 -> {
                                     System.out.println("Enroll myself to a Course");
                                     System.out.println("Enter the course code:");
                                     String courseCode = input.next();
@@ -67,84 +67,86 @@ public class Main{
                                     } else {
                                         System.out.println("Course not found, press 2 to view available courses or 4 to retry");
                                     }
-                                } catch (Exception e) {
-                                    System.out.println("Invalid input, press 2 to view available courses or 4 to retry");
+                                    break;
                                 }
-                                break;
-                            }
-                            case 5 -> {
-                                System.out.println("Unroll myself from a Course");
-                                System.out.println("Enter the course code:");
-                                String courseCode = input.next();
-                                Course Ucourse = courseMap.get(courseCode);
-                                if (Ucourse != null) {
-                                    course.unenrollCourse(Ucourse);
-                                } else {
-                                    System.out.println("Course not found");
+                                case 5 -> {
+                                    System.out.println("Unroll myself from a Course");
+                                    System.out.println("Enter the course code:");
+                                    String courseCode = input.next();
+                                    Course Ucourse = courseMap.get(courseCode);
+                                    if (Ucourse != null) {
+                                        course.unenrollCourse(Ucourse);
+                                    } else {
+                                        System.out.println("Course not found");
+                                    }
+                                    break;
                                 }
-                                break;
+                                case 0 -> {
+                                    System.out.println("Thank you for using the Course Scheduler");
+                                    break;
+                                }
+                                default -> {
+                                    System.out.println("Invalid input");
+                                    break;
+                                }
                             }
-                            case 0 -> {
-                                System.out.println("Thank you for using the Course Scheduler");
-                                break;
-                            }
-                            default -> {
-                                System.out.println("Invalid input");
-                                break;
-                            }
+                            System.out.println(Messages.studentMessage);
                         }
-                        System.out.println(Messages.studentMessage);
                     }
-                }
-                case 2 -> {
-                    System.out.println(Messages.facultyMessage);
-                    System.out.println();
-                    Faculty faculty = new Faculty();
-                    faculty.facultyINFO();
-                    logInOption = input.nextInt();
-                    while (logInOption != 0) {
+                    case 2 -> {
+                        System.out.println(Messages.facultyMessage);
+                        System.out.println();
+                        Faculty faculty = new Faculty();
+                        faculty.facultyINFO();
                         logInOption = input.nextInt();
-                        switch (logInOption) {
-                            case 1 -> {
-                                System.out.println("View Course Schedule");
-                                break;
-                            }
-                            case 2 -> {
-                                System.out.println("View My Course List");
-                                course.displayCourses();
-                                break;
-                            }
-                            case 3 -> {
-                                System.out.println("View My Information");
-                                faculty.displayFaculty();
-                                break;
-                            }
-                            case 4 -> {
-                                System.out.println("Assign myself to a Course");
-                                break;
-                            }
-                            case 5 -> {
-                                System.out.println("Remove myself from a Course");
-                                break;
-                            }
-                            case 0 -> {
-                                System.out.println("Thank you for using the Course Scheduler");
-                                break;
-                            }
-                            default -> {
-                                System.out.println("Invalid input");
-                                break;
+                        while (logInOption != 0) {
+                            logInOption = input.nextInt();
+                            switch (logInOption) {
+                                case 1 -> {
+                                    System.out.println("View Course Schedule");
+                                    break;
+                                }
+                                case 2 -> {
+                                    System.out.println("View My Course List");
+                                    course.displayCourses();
+                                    break;
+                                }
+                                case 3 -> {
+                                    System.out.println("View My Information");
+                                    faculty.displayFaculty();
+                                    break;
+                                }
+                                case 4 -> {
+                                    System.out.println("Assign myself to a Course");
+                                    break;
+                                }
+                                case 5 -> {
+                                    System.out.println("Remove myself from a Course");
+                                    break;
+                                }
+                                case 0 -> {
+                                    System.out.println("Thank you for using the Course Scheduler");
+                                    break;
+                                }
+                                default -> {
+                                    System.out.println("Invalid input");
+                                    break;
+                                }
                             }
                         }
                     }
+                    case 0 -> {
+                        System.out.println("Thank you for using the Course Scheduler");
+                        break;
+                    }
+                    default -> {
+                        System.out.println("Invalid input");
+                        break;
+                    }
                 }
-                case 0 -> {
-                    System.out.println("Thank you for using the Course Scheduler");
-                    break;
-                }
-                default -> { System.out.println("Invalid input");
-                    break;
-                }
+            }catch (InputMismatchException e ) {
+                System.out.println("Invalid input");
+                input.next();
             }
         }
     }
